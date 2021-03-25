82eff17e9328cf7e9f176aaa33b7d214126f818d
lijithavarma <lijithavarma98@gmail.com>
2021-03-25 10:02:11 +0530

    First Commit

---
+++ first-maven-app/src/main/java/com/lti/model/Employee.java
@@ -0,0 +1,28 @@
+package com.lti.model;
+
+public class Employee {
+	String empId;
+	String empName;
+	
+	public String getEmpid(){
+		return empId;
+		}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+
+}
